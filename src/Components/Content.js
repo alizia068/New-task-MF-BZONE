936583515448140f03adae2d1456d6047013d6b0
ali zia <alizia068@gmail.com>
2023-11-16 23:41:29 +0500

    Test task has been completed

---
+++ src/Components/Content.js
@@ -0,0 +1,96 @@
+import React from "react";
+import '../Styles/Content.css'
+const Content = () => {
+    return (
+        <>
+            <div>
+                <div className="main-txt">
+                    <span className="text-dec">At Procore Estimators, our area of expertise lies in delivering precise and dependable mechanical and plumbing estimates. Our seasoned team of estimators grasps the complexities of mechanical and plumbing systems, offering all-encompassing and thorough estimates for your projects. Catering to both residential and commercial constructions, we factor in equipment, materials, labor, and project-specific requirements. Our unwavering commitment to providing vital information enables our clients to make well-informed decisions to ensure the success of their projects. Our mechanical and plumbing estimates form the bedrock of a triumphant construction project.</span>
+                </div>
+                <div >
+                    <img className="group501-image" src="./Group 501.png" alt="logo" />
+                </div>
+                <div>
+                    <h1 className="heading-txt">Plumbing Estimation Data</h1>
+                </div>
+
+                <div className="group445-image">
+                    <img className="group501-image" src="./Group 446.png" alt="logo" />
+                </div>
+
+                {/* 2 */}
+                <div>
+                    <h2 className="heading-txt">Benefits of Mechanical Estimating</h2>
+                </div>
+                <div className="img_txt-container">
+
+                    <div className="mechanical_estimation-style">
+                        <div className="mechanical_list-txt">
+                            <li>Getting access to the industry standards insights.</li>
+                            <li>Understanding the internal project cost.</li>
+                            <li>Getting the most reliable mechanical estimates.</li>
+                            <li>Increasing the probability of high profit.</li>
+                            <li>Boosting their win-bid ratio.</li>
+                            <li>Streamlining their project activities with more confidence.</li>
+                            <li>Identifying the errors and flaws in their mechanical project.</li>
+                            <li>Mitigating the potential risk while saving their time and money.</li>
+                        </div>
+                        <div>
+                            <div>
+                                <h2 className="heading-txt">Our Plumbing Estimating Services</h2>
+                            </div>
+
+                            {/* 1 */}
+
+                            <div className="plumbing_estimation-style">
+                                <div className="plumbing-list-txt">
+                                    <li>Cold Water Pipes</li>
+                                    <li>Hot Water Pipes</li>
+                                    <li>Vent Pipes</li>
+                                    <li>Gas Pipes</li>
+                                    <li>Sanitary Pipes</li>
+                                    <li>Underground Pipes</li>
+                                    <li>Pipe Fittings</li>
+                                </div>
+                                <div className="plumbing_2-list-txt">
+                                    <li>Pipe Concealment</li>
+                                    <li>Insulations</li>
+                                    <li>Valves</li>
+                                    <li>Hangers</li>
+                                    <li>Struts</li>
+                                    <li>Supports</li>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* ..... */}
+
+                    <div >
+                        <img className="image-set" src="./Rectangle 247.png" alt="rectangle" />
+
+                    </div>
+
+                </div>
+
+                {/* nxt */}
+
+
+                <div className="txt-image">
+                    <img className="second_last_footer" src="./Group 518.png" alt="txt" />
+                </div>
+                <div className="txt-image">
+                    <img className="second_last_footer" src="./Group 398.png" alt="txt" />
+                </div>
+                <div className="txt1-image">
+                    <img className="footer-image" src="./Group 349.png" alt="txt" />
+                </div><div className="txt1-image">
+                    <img className="footer-image" src="./Footer.png" alt="txt" />
+                </div>
+
+            </div>
+
+        </>
+
+    )
+}
+export default Content;
---
+++ src/Components/header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import '../Styles/header.css'
+// import { image } from '../../public/logo (5) 1.png'
+const Header = () => {
+    return (
+        <>
+            <div >
+                <img className="image-size" src="./Group 350.png" alt="logo" />
+
+            </div>
+
+            {/* NavBar */}
+            <div >
+                <img className="image-size" src="./nave bar.png" alt="logo" />
+
+            </div>
+            <div class="img_container">
+                <div class="text-on-img">
+                    <h1>Mechanical/Plumbing</h1>
+                </div>
+            </div>
+
+
+
+        </>
+    )
+}
+export default Header;
